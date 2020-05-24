ec5f880d8835e5c12bcb254629e35e127d65aeb1
apangeles_yanco <angeles_yanco@hotmail.com>
2020-05-23 21:01:05 -0500

    Part_006

--- MyVet.Web/Data/Entities/Manager.cs
+++ MyVet.Web/Data/Entities/Manager.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace MyVet.Web.Data.Entities
 {
-    public class Owner
+    public class Manager
     {
         public int Id { get; set; }
+
         public User User { get; set; }
-        public ICollection<Pet> Pets { get; set; }
-        public ICollection<Agenda> Agendas { get; set; }
     }
 }
