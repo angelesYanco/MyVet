5798c77449f3cc6bff7d6b50a40ecc1d6589be94
apangeles_yanco <angeles_yanco@hotmail.com>
2020-05-23 22:24:27 -0500

    Part_007

---
+++ MyVet.Web/Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Data.Entities;
+
+namespace MyVet.Web.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Owner> Owner { get; set; }
+
+        public DbSet<Manager> Manager { get; set; }
+    }
+}
--- MyVet.Web/Data/Entities/User.cs
+++ MyVet.Web/Data/Entities/User.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
-
 
 namespace MyVet.Web.Data.Entities
 {
