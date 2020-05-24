2e482c808c042e8494c6c5ee3baa7d1b2d5063ae
apangeles_yanco <angeles_yanco@hotmail.com>
2020-05-24 14:38:38 -0500

    Part_009_1

---
+++ MyVet.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyVet.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
